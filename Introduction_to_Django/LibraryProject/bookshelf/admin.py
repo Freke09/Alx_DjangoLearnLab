b8ac59875acff102178e59b7311c606a07925d68
Ndifreke Ndue <ndifrekeh@gmail.com>
2025-08-31 21:31:43 +0100

    modified admin.py and models.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'published_date')
-    list_filter = ('published_date')
-    
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ['publication_year']
+    search_fields = ('title', 'author')
+
 
 admin.site.register(Book, BookAdmin)
 # Register your models here.
--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -3,10 +3,10 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    published_date = models.IntegerField()
+    publication_year = models.IntegerField()
 
 
     def __str__(self):
-        return f"{self.title} by {self.author} Published in {self.published_date}"
+        return f"{self.title} by {self.author} Published in {self.publication_year}"
 
 # Create your models here.
