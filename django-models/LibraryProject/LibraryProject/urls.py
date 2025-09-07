da36d0abb79e996570b5d79d377c0b946349ec95
Ndifreke Ndue <ndifrekeh@gmail.com>
2025-09-07 19:56:18 +0100

    views and url configuration

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from relationship_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', views.list_books, name='list_books'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Author, Book, Library, Librarian
 
-# Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    book_list = ", ".join([book.title for book in books])
+    return HttpResponse(f"Books: {book_list}")
