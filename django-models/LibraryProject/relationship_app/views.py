e7c137fc15daa1787e7a53638dff08272c10afba
Ndifreke Ndue <ndifrekeh@gmail.com>
2025-09-07 20:06:40 +0100

    modified views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,5 @@ from django.http import HttpResponse
 from .models import Author, Book, Library, Librarian
 
 def list_books(request):
-    books = Book.objects.select_related("author").all()
-    context = {"books": books}
-    return render(request, "relationship_app/book_list.html", context)
+    books = Book.objects.all()
+    return render(request, "relationship_app/book_list.html", {"books": books})
